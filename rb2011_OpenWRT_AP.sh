40d8f49e8540663a047d40fe2aaad5f37501447f
el-spirit <32513814+el-spirit@users.noreply.github.com>
2026-01-14 23:13:19 +0300

    Update rb2011_OpenWRT_AP.sh

--- rb2011_OpenWRT_AP.sh
+++ rb2011_OpenWRT_AP.sh
@@ -21,35 +21,5 @@ uci set dhcp.lan.ignore='1'
 uci commit dhcp
 /etc/init.d/dnsmasq restart
 
-# ---------- Wi-Fi ----------
-# Настройки радиоинтерфейса (2.4 GHz)
-SSID="ChikaWiFi"
-PASSWORD="irdiS0066"
-COUNTRY="RU"
-CHANNEL="13"  # подбери свободный канал
-
-opkg update
-opkg remove wpad-basic wpad-mini 2>/dev/null
-opkg install wpad
-
-uci set wireless.radio0.disabled='0'
-uci set wireless.radio0.country="$COUNTRY"
-uci set wireless.radio0.channel="$CHANNEL"
-uci set wireless.radio0.hwmode='11ng'
-uci set wireless.radio0.htmode='HT20'
-
-uci set wireless.@wifi-iface[0].device='radio0'
-uci set wireless.@wifi-iface[0].mode='ap'
-uci set wireless.@wifi-iface[0].network='lan'
-uci set wireless.@wifi-iface[0].ssid="$SSID"
-uci set wireless.@wifi-iface[0].encryption='psk2'
-uci set wireless.@wifi-iface[0].key="$PASSWORD"
-
-uci set wireless.@wifi-iface[0].disassoc_low_ack='1'  # стабильность для старого AP
-
-uci commit wireless
-wifi reload
-
 # ---------- ФИНАЛ ----------
 echo "RB2011 настроен как простая точка доступа"
-echo "LAN bridge готов, DHCP отключен, Wi-Fi SSID: $SSID"
